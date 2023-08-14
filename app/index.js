a738845638edf5086faabb27c81dfc7d629b631a
starydv7 <55851664+starydv7@users.noreply.github.com>
2023-08-14 15:54:10 +0530

    getting starte d page

--- app/index.js
+++ app/index.js
@@ -1,10 +1,25 @@
-import{View,Text}from "react-native";
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import styles from "./styles";
 
-const Home=()=>{
-    return(
-        <View>
-            <Text>Pawan Yadav</Text>
-        </View>
-    )
-}
+const Home = () => {
+  const handleGetStarted = () => {
+    // Handle the action when the "Get Started" button is pressed
+    console.log("Get Started button pressed");
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={styles.welcome}>Welcome</Text>
+      <Text style={styles.welcome1}>Browse, connect and discover kingenic offerings and resources</Text>
+      <TouchableOpacity style={styles.button} onPress={handleGetStarted}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
+      <Text style={styles.normalText}>
+        Some normal text below the button
+      </Text>
+    </View>
+  );
+};
+
 export default Home;
---
+++ app/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#4287f5",
+    padding: 15,
+    borderRadius: 8,
+    marginBottom: 40,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+   
+  },
+  normalText: {
+    fontSize: 16,
+    textAlign: "center",
+    marginBottom:-300,
+  },
+  welcome:{
+    fontSize:34,
+    color:"black",
+    fontWeight:"bold",
+    textAlign:"center",
+    marginBottom:-50,
+
+  },
+  welcome1:{
+    fontSize:16,
+    color:"black",
+    fontWeight:400,
+    textAlign:"center",
+    padding:50,
+
+  }
+});
+
+export default styles;
