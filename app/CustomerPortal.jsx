e378210aeb3a3a7b884eabe24f2ac499f8570bc8
starydv7 <55851664+starydv7@users.noreply.github.com>
2023-08-18 17:13:24 +0530

    page for Authentication
    Authentication

---
+++ app/CustomerPortal.jsx
@@ -0,0 +1,12 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+const MyComponent = () => {
+    return (
+        <View style={styles.container}>
+            <Text>MyComponent</Text>
+        </View>
+    );
+};
+
+export default MyComponent;
